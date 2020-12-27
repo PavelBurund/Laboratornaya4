52c1a2cfb4bea92736aff5a59274e984c56e0685
Павел Бурундуков <>
2020-12-27 18:04:28 +0300

    корректировки

---
+++ BobrovBurundukovLabFour/BobrovBurundukovLabFour/BobrovBurundukovLabFour.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include "ShapeAccumulator.h"
+#include "Shapes.h"
+
+int main()
+{
+	std::list<Shape*> shapes;
+	shapes.push_back(new Circle(3));
+	shapes.push_back(new Rectangle(4, 5));
+	shapes.push_back(new Square(5));
+	shapes.push_back(new Triangle(3, 4, 5));
+	int sumOfArea = 0;
+	Shape* maxAreaOfFigures = nullptr;
+	Shape* maxPerimeterOfFigures = nullptr;
+	Shape* minAreaOfFigures = nullptr;
+	Shape* minPerimeterOfFigures = nullptr;
+	for (auto iter = shapes.begin(); iter != shapes.end(); ++iter)
+	{
+		sumOfArea += (*iter)->calcArea();
+		if (maxAreaOfFigures == nullptr || maxAreaOfFigures->calcArea() < (*iter)->calcArea()) maxAreaOfFigures = *iter;
+		if (minAreaOfFigures == nullptr || minAreaOfFigures->calcArea() > (*iter)->calcArea()) minAreaOfFigures = *iter;
+		if (maxPerimeterOfFigures == nullptr || maxPerimeterOfFigures->calcPerimeter() < (*iter)->calcPerimeter()) maxPerimeterOfFigures = *iter;
+		if (minPerimeterOfFigures == nullptr || minPerimeterOfFigures->calcPerimeter() > (*iter)->calcPerimeter()) minPerimeterOfFigures = *iter;
+	}
+	std::vector<Shape*> shapes2;
+	shapes2.push_back(new Circle(3));
+	shapes2.push_back(new Rectangle(5, 6));
+	shapes2.push_back(new Square(50));
+	shapes2.push_back(new Triangle(3, 4, 5));
+	ShapeAccumulator shac;
+	shac.AddShapes(shapes);
+	shac.AddShapes(shapes2);
+	shac.AddShape(new Circle(6));
+	std::cout << shac.getMaxAreaShape() << std::endl;
+	std::cout << shac.getMaxPerimeterShape() << std::endl;
+	std::cout << shac.getTotalPerimeter() << std::endl;
+	std::cout << shac.getTotalArea() << std::endl;
+	std::cout << shac.getMinAreaShape() << std::endl;
+	std::cout << shac.getMinPerimeterShape() << std::endl;
+	return 0;
+}
---
+++ BobrovBurundukovLabFour/BobrovBurundukovLabFour/ShapeAccumulator.h
@@ -0,0 +1,86 @@
+#include "Shapes.h"
+
+class ShapeAccumulator
+{
+private:
+	std::list<Shape*> list;
+public:
+	void AddShape(Shape* shape)
+	{
+		list.push_back(shape);
+	}
+
+	template<typename T>
+	void AddShapes(T&& cont) {
+		for (auto it = std::begin(cont); it != std::end(cont); it++)
+		{
+			AddShape(*it);
+		}
+	}
+
+	Shape* getMaxAreaShape()
+	{
+		auto maxPerimeterShape = *list.begin();
+		for (auto it = list.begin(); it != list.end(); it++)
+		{
+			if ((*it)->calcArea() > maxPerimeterShape->calcArea())
+				maxPerimeterShape = (*it);
+		}
+		return maxPerimeterShape;
+	}
+
+	Shape* getMinAreaShape()
+	{
+		auto minAreaShape = *list.begin();
+		for (auto it = list.begin(); it != list.end(); it++)
+		{
+			if ((*it)->calcArea() < minAreaShape->calcArea())
+				minAreaShape = (*it);
+		}
+		return minAreaShape;
+	}
+
+	Shape* getMaxPerimeterShape()
+	{
+		auto maxPerimeterShape = *list.begin();
+		for (auto it = list.begin(); it != list.end(); it++)
+		{
+			if ((*it)->calcPerimeter() > maxPerimeterShape->calcPerimeter())
+				maxPerimeterShape = (*it);
+		}
+		return maxPerimeterShape;
+	}
+
+	Shape* getMinPerimeterShape()
+	{
+		auto minPerimeterShape = *list.begin();
+		for (auto it = list.begin(); it != list.end(); it++)
+		{
+			if ((*it)->calcPerimeter() < minPerimeterShape->calcPerimeter()) 
+				minPerimeterShape = (*it);
+		}
+		return minPerimeterShape;
+	}
+
+	float getTotalArea()
+	{
+		double sumOfAreas = 0;
+		for (auto it = list.begin(); it != list.end(); it++)
+		{
+			sumOfAreas += (*it)->calcPerimeter();
+		}
+
+		return sumOfAreas;
+	}
+
+	float getTotalPerimeter()
+	{
+		double sumOfPerimeters = 0;
+		for (auto it = list.begin(); it != list.end(); it++)
+		{
+			sumOfPerimeters += (*it)->calcPerimeter();
+		}
+
+		return sumOfPerimeters;
+	}
+};
